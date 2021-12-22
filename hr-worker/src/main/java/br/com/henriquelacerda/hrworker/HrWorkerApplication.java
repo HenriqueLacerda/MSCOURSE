1acb78897eb7df681cb9d03cc8052b7627a469e9
HenriqueLacerda <_Serp8891>
2021-12-22 12:15:33 -0300

    Eureka no projeto hr-worker

--- hr-worker/src/main/java/br/com/henriquelacerda/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/br/com/henriquelacerda/hrworker/HrWorkerApplication.java
@@ -2,8 +2,11 @@ package br.com.henriquelacerda.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HrWorkerApplication {
 
     public static void main(String[] args) {
